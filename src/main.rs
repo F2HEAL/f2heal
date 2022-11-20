89786b6b8a499ba775561d18b3f084a1d0337bd9
zaBuxx <wgo@belgoline.com>
2022-11-20 12:40:57 +0100

    it compiles

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,13 @@ use rand_chacha::ChaCha8Rng;
 use rand::prelude::*;
 use std::f64::consts::PI;
 use std::i16;
-use flac_bound::{WriteWrapper, FlacEncoder};
+//use flac_bound::{WriteWrapper, FlacEncoder};
+use flac_bound;
 use std::fs::File;
+use hound;
+
+use clap::Parser;
+use colored::Colorize;
 
 
 // Golden values:
@@ -27,6 +32,154 @@ const PAUZES       : [i64; 2] = [3, 4];
 
 const SECONDSOUTPUT: i64 = 7200;   // Duration of output wav
 const RANDOMSEED   : u64 = 4;      // Seed to contract random pattern generation
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+/// Create F2Heal audio output
+struct Arguments {
+
+    /// Channels or fingers per side (L/R), 
+    #[arg(short, long, default_value_t = 4)]
+    channels : i64,
+
+    /// Sample rate in Hz
+    #[arg(long, default_value_t = 44100)]
+    samplerate : i64,
+
+    /// Frequency of finger stimulation in Hz
+    #[arg(long, default_value_t = 250)]
+    stimfreq : i64,
+
+    /// Duration of the finger stimulation in ms
+    #[arg(long, default_value_t = 100)]
+    stimperiod : i64,
+
+    /// Duration of one cycle (stimulation of all fingers)
+    #[arg(long, default_value_t = 666)]
+    cycleperiod : i64,
+
+    /// Duration (in cycles) of one pauze-cycle
+    #[arg(long, default_value_t = 5)]
+    pauzecycleperiod : i64,
+
+    /// The cycles (within the pauze-cycle) with no stimulation output produced
+    #[arg(short, long)]
+    pauzes : Vec<i64>,
+
+    /// Duration in sec of output
+    #[arg(short,long, default_value_t = 90)]
+    secondsoutput: i64,
+
+    /// Random seed (default from timer)
+    #[arg(short, long)]
+    randomseed: Option<i64>,
+
+    /// Create WAV outputfile in stead of default FLAC
+    #[arg(short,long, default_value_t = false)]
+    wavoutput: bool,
+
+    /// Output verbosity (multiple allowed)
+    #[clap(short, long, action = clap::ArgAction::Count)]
+    verbosity: u8,
+
+}
+
+impl Arguments {
+    fn verify_argvalues(&self) {
+        // Do the stimulation frequency en period match, otherwise said, does the stimulation sine
+        // end on period end
+
+        let stimfreq_frame = 1000 / self.stimperiod;
+        let smooth_stim_badend = (self.stimfreq % stimfreq_frame) != 0;
+
+        if smooth_stim_badend {
+            println!("\n{}",
+                format!("WARNING: Stimulation period and frequency do not match!").red().bold());
+        }
+
+        for pauze in self.pauzes.iter() {
+            if pauze >= &self.pauzecycleperiod {
+                println!("\n{}",    
+                    format!("WARNING: This pauze will have no effect: {}", pauze).red().bold(),
+                );
+            }
+        }
+    }
+
+
+    fn display_config(&self) {
+        println!("Generating output for:");
+        println!("   Channels [L/R]          : {}", self.channels);
+        println!("   Sample Rate             : {}Hz", self.samplerate);
+        println!("   Duration                : {}s", self.secondsoutput);
+        println!("   Format                  : {}", if self.wavoutput { "WAV" } else { "FLAC"});
+        println!("");
+        println!("   Stimulation details:");
+        println!("     Stimulation Frequency : {}Hz", self.stimfreq);
+        println!("     Stimulation Period    : {}ms", self.stimperiod);
+        println!("     Cycle Period          : {}ms", self.cycleperiod);
+        println!("");
+        if self.pauzes.is_empty() {
+            println!("   Without pauzes");
+        } else {
+            println!("   Pauze cycle period      : {}", self.pauzecycleperiod);
+            println!("   Pauze on cycles         : {:?}", self.pauzes);
+        }
+        println!("");
+        if self.randomseed.is_none() {
+            println!("   Randomized seed");
+        } else {
+            println!("   Random seed             : {}", self.randomseed.unwrap());
+        }
+
+        self.verify_argvalues();
+    }
+
+    fn construct_fname(&self) -> String {
+        let mut result: String = "output/sine-".to_owned();
+
+        result.push_str(&self.stimfreq.to_string());    result.push_str("SFREQ-");
+        result.push_str(&self.stimperiod.to_string());  result.push_str("SPER-");
+        result.push_str(&self.cycleperiod.to_string()); result.push_str("CPER--");
+
+        if !self.pauzes.is_empty() {
+            let mut first : bool = true;
+
+            for pauze in self.pauzes.iter() {
+                if first {
+                    first = false;
+                } else {
+                    result.push_str("_");
+                }
+
+                result.push_str(&pauze.to_string()); 
+            }
+            result.push_str("P");
+            result.push_str(&self.pauzecycleperiod.to_string());
+            result.push_str("--");
+        }
+
+        if !self.randomseed.is_none() {
+            result.push_str(&self.randomseed.unwrap().to_string());
+            result.push_str("RSEED--");
+        }
+
+        result.push_str(&self.channels.to_string());      result.push_str("LR-");
+        result.push_str(&self.samplerate.to_string());    result.push_str("Hz-");
+        result.push_str(&self.secondsoutput.to_string()); result.push_str("s");
+
+        if self.wavoutput {
+            result.push_str(".wav");
+        } else {
+            result.push_str(".flac");
+        }
+
+        result
+    }
+
+}
+
+
 
 type  AtomSeq = [i64; CHANNELS as usize];
 
@@ -124,21 +277,55 @@ impl SeqGen {
         
 }
 
+
 fn main() {
 
+    let args = Arguments::parse();
+    println!("{:?}", args);
+
+    args.display_config();
+
     //set filename with all parameters included
-    let fname = "output/sine-2hands-pauzed-".to_string() + &CHANNELS.to_string() + &"chan-".to_string() 
-    + &STIMFREQ.to_string() + &"SFREQ-".to_string() + &STIMPERIOD.to_string() 
-    + &"SPER-".to_string() + &CYCLEPERIOD.to_string() + &"CPER.flac".to_string();    
+    let fname = args.construct_fname();
+
+    println!("\nWriting output to: {}", fname);
+
+    //let samples_to_go : i64 = SECONDSOUTPUT * SAMPLERATE;
+    let samples_to_go = args.secondsoutput * args.samplerate;
     
-    println!("\nWriting {}sec output to: {}", SECONDSOUTPUT, fname);
+
+    //let mut wav_encoder  = hound::WavWriter::<std::io::BufWriter<File>>::new();
+    let mut wav_encoder  = hound::WavWriter::create("/dev/null", hound::WavSpec { channels: 0, sample_rate: 0, bits_per_sample: 0, sample_format: hound::SampleFormat::Int}).unwrap();
+
+    let mut flac_outfile : File;
+    let mut flac_outwrap : flac_bound::WriteWrapper;
+    let mut flac_encoder = flac_bound::FlacEncoder::new().unwrap().init_stdout().unwrap(); 
     
-    let samples_to_go : i64 = SECONDSOUTPUT * SAMPLERATE;
+    if args.wavoutput {
+        // setup wav stream
+        let wavspec = hound::WavSpec {
+            channels: 2*args.channels as u16,
+            sample_rate: args.samplerate as u32,
+            bits_per_sample: 16,
+            sample_format: hound::SampleFormat::Int,
+        };
+        wav_encoder = hound::WavWriter::create(fname, wavspec).unwrap();
+    } else {
+        flac_outfile = File::create(fname).unwrap();
+        flac_outwrap = flac_bound::WriteWrapper(&mut flac_outfile);
+        flac_encoder = flac_bound::FlacEncoder::new().unwrap()
+        .channels((2*args.channels).try_into().unwrap())
+        .bits_per_sample(16)
+        .total_samples_estimate(samples_to_go as u64)
+        .compression_level(8)
+        .init_write(&mut flac_outwrap)
+        .unwrap();
+    }
+
+
     
-    let mut outf = File::create(fname).unwrap();
-    let mut outw = WriteWrapper(&mut outf);
-    let mut enc = FlacEncoder::new().unwrap().channels(8).bits_per_sample(16).total_samples_estimate(samples_to_go as u64).compression_level(8).init_write(&mut outw).unwrap();
-    eprintln!("{:?}", enc);
+    //let mut enc 
+    //eprintln!("{:?}", enc);
 
     let mut seq1 = SeqGen::new();
     seq1.gen_channelorder();
@@ -159,7 +346,15 @@ fn main() {
                 }
             }
         }
-        enc.process_interleaved(&next_sample,1).unwrap();
+
+        if args.wavoutput { 
+            for chan_sample in next_sample.iter() {
+                wav_encoder.write_sample(*chan_sample as i16).unwrap();
+            }
+
+        } else {
+            flac_encoder.process_interleaved(&next_sample,1).unwrap();
+        }
         seq1.next_sample(); 
     }
 
